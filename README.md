d6a311f3bceef122d254791614cfa9ee5a5b81f7
Roland Bewick <roland.bewick@gmail.com>
2023-05-30 13:37:32 +0700

    feat: create split address for zaps

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# Nostr Photobooth
+Take a photo at the Alby Nostr Photobooth at [BTC Prague](https://www.btcprague.com), tag you and your friends and it'll be published on the official Alby Nostr account. Any zaps will be split and sent to the tagged users in the photo. Tagged users will also receive a badge!
+
 Deployment: https://nostr-photobooth.vercel.app
+
+## Development
+
+`npm install`
+`npm run dev`
---
+++ src/utils/createSplitAddress.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const DONATION_PERCENT = 1;
+const DONATION_ADDRESSES = [
+  'nostrbuild@getalby.com',
+  'sepehr@getalby.com', // nostr-hooks
+];
+
+export async function createSplitAddress(splitAddresses) {
+  const splits = {};
+  for (const address of DONATION_ADDRESSES) {
+    splits[address] = DONATION_PERCENT;
+  }
+  
+  for (const address of splitAddresses) {
+    splits[address] = Math.floor(100 / splitAddresses.length);
+  }
+
+  // ensure split sum does not exceed 100%
+  while (Object.values(splits).reduce((a, b) => a + b, 0) > 100) {
+    const greatestSplit = Object.entries(splits).find(entry => !Object.entries(splits).find(other => entry[1] < other[1]));
+    --splits[greatestSplit[0]];
+  }
+
+  const response = await axios
+      .post('https://splitaddress.fly.dev/splits', splits, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+  if (response.status !== 200) {
+    console.error("Failed to create splits", response);
+    throw new Error("Failed to create split address: " + response.status);
+  }
+
+  return response.data;
+}
